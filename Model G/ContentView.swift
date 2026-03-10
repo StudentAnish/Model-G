ee8c8360bf515becd53c57f223cc717552af7419
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2026-03-10 03:09:14 -0700

    Initial Commit

---
+++ Model G/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Model G
+//
+//  Created by Anish  Sinha on 3/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Model G/Model_GApp.swift
@@ -0,0 +1,17 @@
+//
+//  Model_GApp.swift
+//  Model G
+//
+//  Created by Anish  Sinha on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Model_GApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
