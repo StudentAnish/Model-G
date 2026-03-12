b3e18f4fda8cf3f4a0736dcad74467b4a4bd4b9c
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2026-03-12 14:29:58 -0700

    improved Top3MissionRowView UI to be more like Model A (rounded, cyan border, shadow); added 'play' button to highlight when a mission is 'inPlay' (currently being done)

--- Model G/ContentView.swift
+++ Model G/ContentView.swift
@@ -7,17 +7,43 @@
 
 import SwiftUI
 
+struct Mission: Identifiable {
+    /// Mission ID ( UUID() creates a universally unique ID )
+    private(set) var id = UUID()
+    
+    /// Mission Name
+    var name: String
+    
+    /// when Mission is created
+    private(set) var dateCreated: Date = .now
+    
+    var inPlay: Bool
+}
+
+//rename MainView?
 struct ContentView: View {
+    
+    @State var missions: [Mission] =
+    [Mission(name: "", inPlay: false),
+     Mission(name: "", inPlay: true),
+     Mission(name: "", inPlay: false)]
+    
     var body: some View {
-        VStack(spacing: 100){
-            Text("🐙").font(.system(size: 200))
+        VStack(){
+            
+            Text("🐙")
+                .font(.system(size: 200))
                 .shadow(color: .primary, radius: 10)
             
-            Text("Level: 3").font(.largeTitle)
+            Text("Level 3")
+                .font(.title)
+                .padding(.bottom, 100)
             
-            Top3MissionRowView(number: 1)
-            Top3MissionRowView(number: 2)
-            Top3MissionRowView(number: 3)
+            List($missions, editActions: .move){$mission in
+                Top3MissionRowView(mission: $mission, number: 1)
+                    .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
             
         }
         .padding()
@@ -27,15 +53,33 @@ struct ContentView: View {
 
 struct Top3MissionRowView: View {
     
-    @State var missionName: String = ""
+    @Binding var mission: Mission
     var number: Int
     
     var body: some View{
         HStack{
-            Image(systemName: "\(number).circle")
+            TextField("enter your mission", text: $mission.name)
+                .lineLimit(2)
+        
+       
+            Image(systemName: "play.circle.fill")
                 .font(.system(size: 20))
-            TextField("enter your mission", text: $missionName)
+                .onTapGesture {
+                    withAnimation{
+                        mission.inPlay.toggle()
+                    }
+                }
+            
         }
+        .frame(maxWidth: .infinity)
+        .padding(20) //internal padding
+        .background(Color(.systemBackground)).cornerRadius(100)
+        .shadow(color: .cyan, radius: (mission.inPlay ? 5 : 0) )
+        .overlay(
+            RoundedRectangle(cornerRadius: 1000).stroke(Color.cyan, lineWidth: (mission.inPlay ? 2 : 1.5)))
+        
+        
+        .padding(.vertical, 5) //external padding
     }
 }
 
@@ -44,3 +88,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/** Notes:
+ 
+ Why Mission is a struct instead of a class:
+ – when the contents of a struct is updated, a new copy of the struct is created and then saved in the old struct's place. If an array of these structs is also declared Published, then Published detects this change (as the copying behaviour changes the array, whereas an array of classes would not be changed as only the property would be updated) triggering the view to update
+ – modifiying a property in an object (class) in an array does not update the array, whereas updating a property in a struct does update the array
+ 
+ **/
