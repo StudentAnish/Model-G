76a4494b650e06826e201568abcd6393e5d66727
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2026-03-13 13:31:20 -0700

    AmbientStoryView: added test images (Dr. Seuss green eggs and ham) for ambient story and added an arrow to cycle through the story

--- Model G/AmbientStoryView.swift
+++ Model G/AmbientStoryView.swift
@@ -9,27 +9,90 @@ import SwiftUI
 
 struct AmbientStoryView: View {
     var body: some View {
+        view1()
+    }
+}
+
+struct view0: View {
+    
+    @State var pageNum: Int = 0
+    
+    var body: some View {
         ZStack{
-            Image(systemName: "square.split.diagonal.2x2")
+            Image("page-\(pageNum)")
+                .resizable()
+//                .scaledToFit()
+                .scaledToFill()
+                //.aspectRatio(contentMode: .fit)
+            
+            VStack(alignment: .center){
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Button{
+                        withAnimation{
+                            pageNum = (pageNum + 1) % 5
+                        }
+                    } label: {
+                        Image(systemName: "arrow.up.right.square.fill")
+                            
+                    }
+                    .buttonStyle(.plain)
+                    .foregroundColor(.black)
+                    .shadow(radius: 0)
+                    .rotationEffect(Angle(degrees: 45))
+                    .font(.system(size: 50))
+                    Spacer()
+                }
+            }.padding(.bottom, 25)
+        }.ignoresSafeArea()
+    }
+}
+
+struct view1: View {
+    
+    @State var pageNum: Int = 0
+    
+    var body: some View {
+        VStack{
+//            Image(systemName: "square.split.diagonal.2x2")
+            Image("page-\(pageNum)")
+                .resizable()
+                //.aspectRatio(contentMode: .fit)
                 .scaledToFill()
             
-            VStack(spacing: 100){
+            //Spacer()
                 
-                Text("Story Text")
-                
-                
-                Button{
-                    
-                } label: {
-                    
+            //Text("Story Text")
+            
+            Button{
+                withAnimation{
+                    pageNum = (pageNum + 1) % 5
                 }
-                
-                NavigationLink(destination: ContentView()){
-                    Image(systemName: "arrowshape.turn.up.right.fill")
-                        .scaleEffect(y: -1)
-                }
+            } label: {
+                //Image(systemName: "arrow.up.right.square.fill")
+                //Image(systemName: "arrowshape.turn.up.right.fill")
+                Image(systemName: "arrow.forward")
+                    .scaleEffect(y: -1)
+                    
             }
-        }.frame(width: .infinity, height: .infinity)
+            .buttonStyle(.plain)
+            .foregroundColor(.white)
+            //.shadow(radius: 0)
+            .font(.system(size: 50))
+            .padding(.vertical, 10)
+            
+            
+//            NavigationLink(destination: ContentView()){
+//                Image(systemName: "arrowshape.turn.up.right.fill")
+//                    .scaleEffect(y: -1)
+//            }
+            
+            //Spacer()
+        }
+        .background(.black)
+        .frame(width: .infinity, height: .infinity)
         .navigationBarBackButtonHidden(true)
     }
 }
